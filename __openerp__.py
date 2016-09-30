18524daf84e1b9e42812652651594134aeccde10
AfroMonkey <moisalejandro@gmail.com>
2016-09-30 16:55:54 -0500

    [IMP] AxC Contract

--- __openerp__.py
+++ __openerp__.py
@@ -3,8 +3,9 @@
     'version': '1.0',
     'author': 'Humanytek',
     'website': 'http://humanytek.com',
-    'depends': ['purchase'],
+    'depends': ['purchase', 'l10n_mx_invoice_amount_to_text'],
     'data': [
+        'views/axc_contract.xml',
         'views/purchase_contract_type.xml',
     ]
 }
--- models/purchase_contract_type.py
+++ models/purchase_contract_type.py
@@ -13,3 +13,29 @@ class PurchaseContractType(models.Model):
         ('surplus', 'Excedente'),
         ('na', 'No aplica'),
     ], default='na', required=True)
+
+    tons = fields.Float(compute='_compute_tons', store=False)
+    tons_text = fields.Text(compute='_compute_tons_text', store=False)
+    product = fields.Many2one('product.product', compute='_compute_product', store=False)
+
+    @api.one
+    @api.depends('order_line')
+    def _compute_tons(self):
+        self.tons = 0;
+        for line in self.order_line:
+            self.tons = line.product_qty
+            break
+
+    @api.one
+    @api.depends('tons')
+    def _compute_tons_text(self):
+        self.tons_text = "TODO"
+
+    @api.one
+    @api.depends('order_line')
+    def _compute_product(self):
+        product = False
+        for line in self.order_line:
+            product = line.product_id
+            break
+        self.product = product
